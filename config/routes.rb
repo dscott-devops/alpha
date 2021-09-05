218911a5b649452e2898f58eb448e4ee673a13f6
Darryl Scott <dscott@rts-llc.com>
2021-09-05 20:35:52 +0000

    Adding Articles listing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'page#index'
   get 'page', to: 'page#index'
   get 'page', to: 'page#contact'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
