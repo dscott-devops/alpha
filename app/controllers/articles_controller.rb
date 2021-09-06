bf52e86fc65feb8406cc73e464d6b6381f15fc59
Darryl Scott <dscott@rts-llc.com>
2021-09-06 16:26:34 +0000

    Added Destroy, added all routes, added edit function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def new
     @article = Article.new
@@ -18,5 +21,19 @@ class ArticlesController < ApplicationController
   else
     render 'new'
   end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description,:tags,:category))
+      flash[:notice] = "Article was successfully updated"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
   end
 end
