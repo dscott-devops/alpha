fae27e01c9f5bb2e0eabeca277b41d4278cc8a83
Darryl Scott <dscott@rts-llc.com>
2021-09-07 21:28:30 +0000

    Adding user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: {case_sensitive: false },
+   length: {minimum: 3, maximum:25}
+  validates :email, presence: true, uniqueness: {case_sensitive: false },
+   length: {minimum: 10, maximum:125}
 end
