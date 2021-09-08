ddfbccc6a14c4368ec9a11e877ad611933dbe825
Darryl Scott <dscott@rts-llc.com>
2021-09-08 10:40:23 +0000

    Added user code to save users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ def new
   @user = User.new
 end
 
+def create
+  @user = User.new(user_params)
+  if @user.save
+    flash["notice"] = "You have successfully created a user #{@user.first_name} #{@user.last_name} "
+    redirect_to articles_path
+  else
+    render 'new'
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :first_name,:last_name, :role, :email,:password)
+end
+
 end
